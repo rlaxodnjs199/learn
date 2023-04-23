5bf46a11c2d37f83ef95e79b013db29927a532de
rlaxodnjs199 <rlaxodnjs199@gmail.com>
2023-04-22 19:56:42 -0500

    Validators, Custom validator, validator.js

--- nodejs/udemy-lecture/models/tourModel.js
+++ nodejs/udemy-lecture/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,9 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name.'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have less or equal than 10 characters'],
+      // validate: [validator.isAlpha, 'A tour name must only contain characters'],
     },
     slug: {
       type: String,
@@ -23,10 +27,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty.'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -36,10 +46,22 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price.'],
     },
-    priceDiscount: Number,
-    // remove all white space at the beginning and at the end.
+    priceDiscount: {
+      type: Number,
+      // custom validator
+      validate: {
+        validator: function (val) {
+          // 'this' only points to current doc on NEW document creation.
+          // Does not work on update.
+          return val < this.price;
+        },
+        // {VALUE}: same as val, provided by mongoose.
+        message: 'Discount price ({VALUE}) should be below the regular price.',
+      },
+    },
     summary: {
       type: String,
+      // remove all white space at the beginning and at the end.
       trim: true,
     },
     description: {
