60823b66ef211a2b242f14ef8e52e5327f4e350f
rlaxodnjs199 <rlaxodnjs199@gmail.com>
2023-04-24 21:24:06 -0500

    Error handling-2

--- nodejs/udemy-lecture/controllers/errorController.js
+++ nodejs/udemy-lecture/controllers/errorController.js
@@ -1,9 +1,66 @@
+const AppError = require('../utils/appError');
+
+// handle DB CastError: Operation Error
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Duplicate field value: ${err.keyValue.name}. Please use another value.`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errMessages = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errMessages.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    // Operational, trusted error
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Unknown error: we don't want to leak error details
+    // 1) log error on the serverside
+    console.error('ERROR', err);
+    // 2) send a general error message to the client
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    // let error = {...err} doesn't work
+    // because it does not set the prototype
+    // while Object.assign sets the prototype.
+    let error = Object.assign(err);
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
+  }
 };
--- nodejs/udemy-lecture/server.js
+++ nodejs/udemy-lecture/server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+// globally handle uncaught exception
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 // load environment variables from the path
 dotenv.config({ path: './config.env' });
 
@@ -12,6 +19,15 @@ mongoose.connect(db).then(() => {
 // app needs to be loaded after loading an environment variable file.
 const app = require('./app');
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}...`);
+});
+
+// globally handle unhandled promise rejection
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  // gracefully shutdown the server
+  server.close(() => {
+    process.exit(1);
+  });
 });
