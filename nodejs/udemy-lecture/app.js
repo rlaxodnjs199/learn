2daf0fb3220e0468fa6bf725ca1784e512ad2f7a
rlaxodnjs199 <rlaxodnjs199@gmail.com>
2023-04-29 21:04:45 -0500

    Set security HTTP headers and limit request body size

--- nodejs/udemy-lecture/app.js
+++ nodejs/udemy-lecture/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
@@ -8,6 +9,9 @@ const userRouter = require('./routes/userRoutes');
 const errorHandler = require('./controllers/errorController');
 
 const app = express();
+
+// Set security HTTP headers
+app.use(helmet());
 
 // morgan: used for logging middleware
 // GET /public/overview.html 404 1.380 ms - 159
@@ -26,7 +30,8 @@ app.use('/api', limiter);
 
 // middleware: function that can modify incoming request
 // express.json(): parse incoming requests with JSON payloads
-app.use(express.json());
+// limit body size to 10kb
+app.use(express.json({ limit: '10kb' }));
 
 // import static file folder
 app.use(express.static(`${__dirname}/public`));
