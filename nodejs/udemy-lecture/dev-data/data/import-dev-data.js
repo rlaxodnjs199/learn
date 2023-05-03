577b7faaa0fc5fa752b40c6d30363a51e8aaf48b
rlaxodnjs199 <rlaxodnjs199@gmail.com>
2023-05-03 16:15:02 -0500

    Import more data

--- nodejs/udemy-lecture/dev-data/data/import-dev-data.js
+++ nodejs/udemy-lecture/dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 // load environment variables from the path
 dotenv.config({ path: './config.env' });
@@ -14,11 +16,16 @@ mongoose.connect(db).then(() => {
 
 // READ JSON File
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
-
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 // Import data into DB
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded!');
     process.exit();
   } catch (err) {
@@ -30,6 +37,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted!');
     process.exit();
   } catch (err) {
